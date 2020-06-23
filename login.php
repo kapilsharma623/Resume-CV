b9307be36047d29f6d994698f3e0ec566681ab67
Kapil Sharma <65452458+kapilsharma623@users.noreply.github.com>
2020-06-23 14:32:44 +0530

    update 1

---
+++ login.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<meta name="Description" content="Enter your description here"/>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
+<link href="css/styles.css" rel="stylesheet" />
+<title>Admin</title>
+
+</head>
+<body>
+<div class="container">
+<div class="loginbox">
+    <h1>Admin Login</h1>
+    <form action="datapanel.php" method="post"> 
+  <div class="form-group">
+    <input type="username" class="form-control" id="exampleInputEmail1"  placeholder="Enter Username" name="username">
+  </div>
+  <div class="form-group">
+    <input type="password" class="form-control" id="exampleInputPassword1"  placeholder="Enter Password" name="password">
+  </div>
+  <button type="submit" class="btn btn-primary" name="submit">Submit</button>
+</form>
+<?php
+
+
+
+
+?>
+
+</div>
+</div>
+
+
+    
+
+
+
+
+
+
+
+
+
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.1/umd/popper.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.min.js"></script>
+</body>
+</html>
